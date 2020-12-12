6a2965ca2af9552cbedf0ee264611d2acbe8bb59
Hamza <62620529+HamzaELhaissouf@users.noreply.github.com>
2020-12-12 16:32:23 +0100

    disable process timeout

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * Location: /app/Http/Middleware
+ */
+
 namespace App\Http\Middleware;
 
 use Closure;
@@ -19,7 +23,6 @@ class CorsMiddleware
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
             'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
 
