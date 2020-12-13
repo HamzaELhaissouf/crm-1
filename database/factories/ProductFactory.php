d8c89bae5c29d848d87b74ade4db30ff6524dc4c
Hamza <62620529+HamzaELhaissouf@users.noreply.github.com>
2020-12-13 13:12:43 +0100

    add products factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'designation' => $this->faker->name,
+            'prix_de_vente' => $this->faker->numberBetween(10, 100),
+            'stock_initial' => $this->faker->numberBetween(201, 1381),
+            'stock_actuel' => $this->faker->numberBetween(21, 901),
+            'prix_de_dachat' => $this->faker->numberBetween(10, 89),
+            'montant' => $this->faker->numberBetween(12, 129),
+            'image' => $this->faker->imageUrl(),
+        ];
+    }
+}
