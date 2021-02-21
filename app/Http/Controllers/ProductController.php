04b453b2c7e9853adc53f10753b1721b6e4d9417
Hamza <62620529+HamzaELhaissouf@users.noreply.github.com>
2021-02-21 17:38:12 +0100

    fix products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,7 +26,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = DB::table('products')
-            ->orderBy('trending', 'desc')
+            ->orderBy('designation', 'desc')
             ->get(); // TODO: ->paginate(15);
 
         /*
