e1e1a14a0d53c426a75bfa6f6bf15e23087148d7
Hamza <62620529+HamzaELhaissouf@users.noreply.github.com>
2021-03-20 15:52:46 +0100

    change migration definitions

--- database/migrations/2020_12_06_215548_create_users_table.php
+++ database/migrations/2020_12_06_215548_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique()->notNullable();
+            $table->string('email' , 190)->unique()->notNullable();
             $table->string('password');
             $table->timestamps();
         });
